57251f62766a23878978d11ba2087e1c1e2537d1
Saini Kumara <saini@iki.fi>
2018-03-25 23:13:35 +0300

    viikko2 maksukortin testit ja testauskonffit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -12,9 +12,50 @@ public class MaksukorttiTest {
     public void setUp() {
         kortti = new Maksukortti(10);
     }
-
+    
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void konstruktoriAsettaaSaldonOikein() {
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(32);
+        assertEquals(42, kortti.saldo());
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinJosRahaaTarpeeksi() {
+        kortti.lataaRahaa(230);
+        kortti.otaRahaa(240);
+        assertEquals(0, kortti.saldo());
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaTrueJosRahaaTarpeeksi() {
+        kortti.lataaRahaa(230);
+        assertEquals(true, kortti.otaRahaa(240));
+    }
+    
+    @Test
+    public void saldoEiMuutuElleiRahaaTarpeeksi() {
+        kortti.otaRahaa(240);
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaFalseElleiRahaaTarpeeksi() {
+        assertEquals(false, kortti.otaRahaa(240));
+    }
+    
+    @Test
+    public void toStringToimiiOikein() {
+        kortti.lataaRahaa(250);
+        assertEquals("saldo: " + 260/100 + "." + 260%100, kortti.toString());
     }
 }
